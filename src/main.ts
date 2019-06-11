763c9802cd4a211e1e743efba0253e950d6bb11e
Kinrany <kinrany@yandex.com>
2019-06-11 09:14:36 +0300

    Move canvas element to html file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import {create_engine, create_scene} from './babylon';
 
-const canvas = document.createElement('canvas');
-canvas.classList.add('canvas');
-document.body.appendChild(canvas);
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
 const engine = create_engine(canvas);
 const scene = create_scene(engine);
